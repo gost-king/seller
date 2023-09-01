ff6beef3516002f77701ea39a463f244ea38550a
gost-king <143776715+gost-king@users.noreply.github.com>
2023-09-01 11:47:29 +0530

    Update general.js

--- commands/general.js
+++ commands/general.js
@@ -35,99 +35,6 @@ const speed = require('performance-now')
 const fetch = require('node-fetch');
 //---------------------------------------------------------------------------
 cmd({
-    pattern: "chat",
-    react: "🗣️",
-    alias :['gpt'],
-    desc: "chat with an AI(GPT)",
-    category: "AI",
-    use: '<Hii,Nero>',
-    filename: __filename,
-},
-async(Void, citel,text) => {
-    let zx = text.length;
-    if (zx < 8) {
-        let {data} = await axios.get(`http://api.brainshop.ai/get?bid=167991&key=aozpOoNOy3dfLgmB&uid=[${citel.sender.split("@")[0]}]&msg=[${text}]`);
-        return citel.reply(data.cnt);  
-    }
-    if (!text) return citel.reply(`Hey there! ${citel.pushName}. How are you doing these days?`);
-    // const { Configuration, OpenAIApi } = require("openai");
-    // const configuration = new Configuration({
-    //     apiKey: Config.OPENAI_API_KEY || "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",
-    // });
-    // const openai = new OpenAIApi(configuration);
-    // const completion = await openai.createCompletion({
-    //     model: "text-davinci-002",
-    //     prompt: text,
-    //     temperature: 0.5,
-    //     max_tokens: 80,
-    //     top_p: 1.0,
-    //     frequency_penalty: 0.5,
-    //     presence_penalty: 0.0,
-    //     stop: ['"""'],
-    // });
-    // citel.reply(completion.data.choices[0].text);
-    const response = await fetch("https://api.openai.com/v1/chat/completions", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${Config.OPENAI_API_KEY}`,
-    },
-    body: JSON.stringify({
-      model: "gpt-3.5-turbo", 
-      messages: [{ role: "system", content: "You" }, { role: "user", content: text }],
-    }),
-  });
-
-  const data = await response.json();
-  console.log("GPT REPONCE : ",data); 
-  if (!data.choices || data.choices.length === 0) {citel.reply("*Invalid ChatGPT API Key, Please Put New Key*"); }
-  return await  citel.reply(data.choices[0].message.content)
-	
-}
-)
-
-cmd({
-    pattern: "dalle",
-    alias : ['dall','dall-e'],
-    react: "🪂",
-    desc: "Create Image by AI",
-    category: "AI",
-    use: '<an astronaut in mud.>',
-    filename: __filename,
-},
-async(Void, citel,text,{isCreator}) => 
-{
-//if (!isCreator) return citel.reply(tlang().owner)
-if (Config.OPENAI_API_KEY=='') return citel.reply('You Dont Have OPENAI_API_KEY \nPlease Create OPEN API KEY from Given Link \nhttps://platform.openai.com/account/api-keys');
-if (!text) return citel.reply(`*Give Me A Query To Get Dall-E Reponce ?*`); 
-const imageSize = '256x256'
-const apiUrl = 'https://api.openai.com/v1/images/generations';
-const response = await fetch(apiUrl, {
-method: 'POST',
-headers: {
-  'Content-Type': 'application/json',
-  'Authorization': `Bearer ${Config.OPENAI_API_KEY}`
-},
-body: JSON.stringify({
-  model: 'image-alpha-001',
-  prompt: text,
-  size: imageSize ,
-  response_format: 'url'
-})
-});
-
-const data = await response.json();
-let buttonMessage = {
-    image:{url:data.data[0].url},
-    caption : '*---Your DALL-E Result---*'
-
-}
-
-Void.sendMessage(citel.chat,{image:{url:data.data[0].url}})
-}
-)
-    //---------------------------------------------------------------------------
-cmd({
         pattern: "owner",
 	alias: ["owne "],
         desc: "To find owner number",
@@ -148,7 +55,7 @@ cmd({
             contextInfo: {
                 externalAdReply: {
                     title: Config.ownername,
-                    body: '🥷ᴅᴀʀᴋ ɴᴇʀᴏ ᴠ9🥷.',
+                    body: 'DiLSAN-MD.',
                     renderLargerThumbnail: false,
                     thumbnailUrl: ``,
                     thumbnail: log0,
@@ -175,24 +82,18 @@ cmd({
             desc: "is bot alive??"
         },
         async(Void, citel, text, isAdmins) => {
-            let alivemessage = Config.ALIVE_MESSAGE || `_*A bot developed by CYBER-THUSHN*_`
+            let alivemessage = Config.ALIVE_MESSAGE || `_*A bot developed by DiLSAN-MD*_`
             const alivtxt = `
-🔥 *ᴅᴀʀᴋ ɴᴇʀᴏ ᴠ9 ᴍᴀɪɴ ᴄᴏᴍᴍᴀɴᴅ ʟɪꜱᴛ*🔥
+🔥 *ᴍᴀɪɴ ᴄᴏᴍᴍᴀɴᴅ ʟɪꜱᴛ*🔥
 ┏━━━━━━━━━━━━━━━┓
- ❀ *ʜᴇʟʟᴏ ᴡᴇʟᴄᴏᴍᴇ ᴛᴏ ᴅᴀʀᴋ ɴᴇʀᴏ ʙᴏᴛ ᴜꜱᴇʀꜱ*❀
-┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉
 |:̵🤹‍♂️⃟⃪♥️̟⎝⃝࿐ᴜᴘᴛɪᴍᴇ⇨ ${runtime(process.uptime())}
 |:̵🤹‍♂️⃟⃪♥️̟⎝⃝࿐ᴍᴏᴅᴇ⇨ ${Config.WORKTYPE}
 ┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉
-|🤺⃟⃪⃭⃐🥷 ${prefix}1 ♤ᴅᴏᴡɴʟᴏᴀᴅᴇʀ ᴄᴏᴍᴍᴀɴᴅꜱ♤
-|🤺⃟⃪⃭⃐🥷 ${prefix}2 ♤ᴀɴɪᴍᴇ ᴄᴏᴍᴍᴀɴᴅꜱ♤
-|🤺⃟⃪⃭⃐🥷 ${prefix}3 ♤ᴏᴡɴᴇʀ ᴄᴏᴍᴍᴀɴᴅꜱ♤
-|🤺⃟⃪⃭⃐🥷 ${prefix}4 ♤ᴍɪꜱᴄ ᴄᴏᴍᴍᴀɴᴅꜱ♤ 
-|🤺⃟⃪⃭⃐🥷 ${prefix}5 ♤ɢʀᴏᴜᴘ ᴄᴏᴍᴍᴀɴᴅꜱ♤
-|🤺⃟⃪⃭⃐🥷 ${prefix}6 ♤ꜱᴇᴀʀᴄʜ ᴄᴏᴍᴍᴀɴᴅꜱ♤ 
-|🤺⃟⃪⃭⃐🥷 ${prefix}7 ♤ᴏᴛʜᴇʀ ᴄᴏᴍᴍᴀɴᴅꜱ♤
+|🤺⃟⃪⃭⃐🥷 ${prefix}1 ♤ᴏᴡɴᴇʀ ᴄᴏᴍᴍᴀɴᴅꜱ♤ 
+|🤺⃟⃪⃭⃐🥷 ${prefix}2 ♤ɢʀᴏᴜᴘ ᴄᴏᴍᴍᴀɴᴅꜱ♤ 
+|🤺⃟⃪⃭⃐🥷 ${prefix}3 ♤ᴏᴛʜᴇʀ ᴄᴏᴍᴍᴀɴᴅꜱ♤
 ┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉
-©ᴅᴇᴠᴇʟᴏᴘᴇᴅ ʙʏ ᴄʏʙᴇʀ ʏᴀᴋᴜᴢᴀ ᴛᴇᴀᴍ
+©ᴅᴇᴠᴇʟᴏᴘᴇᴅ ʙʏ DiLSAN-MD
 ━━━━━━━━━━━━━━━━
 `;
             let aliveMessage = {
@@ -212,132 +113,8 @@ cmd({
     )
 
 cmd({
-            pattern: "1",
-	    alias: [],
-            react: "",
-            category: "",
-            filename: __filename,
-            desc: "is bot alive??"
-             },
-        async(Void, citel, text) => {
-		try{
-var msg = citel	
-if(!msg.quoted) return 
-if (!msg.quoted.isBaileys ) return 
-if(!msg.quoted.caption) return console.log('ew')
-text = msg.quoted.caption
-if (!text.includes('*ᴅᴀʀᴋ ɴᴇʀᴏ ᴠ9 ᴍᴀɪɴ ᴄᴏᴍᴍᴀɴᴅ ʟɪꜱᴛ*'))  return 
-
-await Void.sendMessage(citel.chat, { react: {  text: "🤹‍♀️", key: msg.key } } )			
-
-        
-            let alivemessage = Config.ALIVE_MESSAGE || `_*A bot developed by MR-HANSAMALA*_`
-            const alivtxt = `
- *ʜᴇʟʟᴏ ${citel.pushName}*
-┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉
-🤺⃟⃪⃭⃐🥷ᴜᴘᴛɪᴍᴇ⇨ ${runtime(process.uptime())}
-🤺⃟⃪⃭⃐🥷ᴍᴏᴅᴇ⇨ ${Config.WORKTYPE}
-┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉
-      ♤ᴅᴏᴡɴʟᴏᴀᴅᴇʀ ᴄᴏᴍᴍᴀɴᴅꜱ♤
-▏▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁
-| 🎋⃟🥷   ɪɴꜱᴛᴀ
-| 🎋⃟🥷   ꜰʙ
-| 🎋⃟🥷   ꜰʙꜱ
-| 🎋⃟🥷   ᴠɪᴅᴇᴏ
-| 🎋⃟🥷   ꜱᴏɴɢ
-| 🎋⃟🥷   ᴛᴛꜱ
-| 🎋⃟🥷   ᴍᴇᴅɪᴀꜰɪʀᴇ
-| 🎋⃟🥷   ʏᴛꜱ
-| 🎋⃟🥷   ʏᴛᴍᴘ3
-| 🎋⃟🥷   ʏᴛᴍᴘ4
-| 🎋⃟🥷   ᴠɪᴅᴇᴏ2
-| 🎋⃟🥷   ʏᴛᴅᴏᴄ
-| 🎋⃟🥷   ᴅᴏᴡɴᴍᴘ4
-| 🎋⃟🥷   ᴘʟᴀʏʟɪꜱᴛ
-| 🎋⃟🥷   ᴛɪᴋᴛᴏᴋ
-┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉
-©ᴅᴇᴠᴇʟᴏᴘᴇᴅ ʙʏ ᴄʏʙᴇʀ ʏᴀᴋᴜᴢᴀ ᴛᴇᴀᴍ
-━━━━━━━━━━━━━━━━
-`;
-            let aliveMessage = {
-                image: {
-                    url: await botpic(),
-                },
-                caption: alivtxt,
-                footer: tlang().footer,
-                headerType: 4,
-            };
-             return Void.sendMessage(citel.chat, aliveMessage, {
-                quoted: citel,
-            });
-
-        }catch(e){
-			
-	}}
-    )
-
-cmd({
-            pattern: "2",
-	    alias: [],
-            react: "📑",
-            category: "general",
-            filename: __filename,
-            desc: "is bot alive??"
-        },
-    async(Void, citel, text) => {
-		try{
-var msg = citel	
-if(!msg.quoted) return 
-if (!msg.quoted.isBaileys ) return 
-if(!msg.quoted.caption) return console.log('ew')
-text = msg.quoted.caption
-if (!text.includes('*ᴅᴀʀᴋ ɴᴇʀᴏ ᴠ9 ᴍᴀɪɴ ᴄᴏᴍᴍᴀɴᴅ ʟɪꜱᴛ*'))  return 
-
-await Void.sendMessage(citel.chat, { react: {  text: "🤹‍♀️", key: msg.key } } )			
-
-        
-            let alivemessage = Config.ALIVE_MESSAGE || `_*A bot developed by MR-HANSAMALA*_`
-            const alivtxt = `
- *ʜᴇʟʟᴏ ${citel.pushName}*
-┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉
-🤺⃟⃪⃭⃐🥷ᴜᴘᴛɪᴍᴇ⇨ ${runtime(process.uptime())}
-🤺⃟⃪⃭⃐🥷ᴍᴏᴅᴇ⇨ ${Config.WORKTYPE}
-┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉
-      ♤ᴀɴɪᴍᴇ ᴄᴏᴍᴍᴀɴᴅꜱ♤
-▏▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁
-| 🎋⃟🥷   ʟᴏʟɪ
-| 🎋⃟🥷   ɴᴇᴋᴏ
-| 🎋⃟🥷   ᴀɴɪᴍᴇ
-| 🎋⃟🥷   ᴍᴀɴɢᴀ
-| 🎋⃟🥷   ᴡᴀʟʟᴘᴀᴘᴇʀ
-| 🎋⃟🥷   ᴀɴɪᴍᴇɴᴇᴡꜱ
-| 🎋⃟🥷   ꜰᴏxɢɪʀʟ
-| 🎋⃟🥷   ɴᴀʀᴜᴛᴏ
-| 🎋⃟🥷   ᴡᴀɪꜰᴜ
-┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉
-©ᴅᴇᴠᴇʟᴏᴘᴇᴅ ʙʏ ᴄʏʙᴇʀ ʏᴀᴋᴜᴢᴀ ᴛᴇᴀᴍ
-━━━━━━━━━━━━━━━━
-`;
-                     let aliveMessage = {
-                image: {
-                    url: await botpic(),
-                },
-                caption: alivtxt,
-                footer: tlang().footer,
-                headerType: 4,
-            };
-             return Void.sendMessage(citel.chat, aliveMessage, {
-                quoted: citel,
-            });
-
-        }catch(e){
-			
-	}}
-    )
-
-cmd({
-    pattern: "3",
-alias: ["ownermenu", "03"],
+    pattern: "1",
+alias: ["ownermenu", "01"],
     react: "📑",
     category: "general",
     filename: __filename,
@@ -396,75 +173,11 @@ await Void.sendMessage(citel.chat, { react: {  text: "🤹‍♀️", key: msg.k
     )
 
 
-cmd({
-    pattern: "4",
-alias: ["miscmenu", "4"],
-    react: "📑",
-    category: "general",
-    filename: __filename,
-    desc: "is bot alive??"
-},
-async(Void, citel, text) => {
-    try{
-var msg = citel	
-if(!msg.quoted) return 
-if (!msg.quoted.isBaileys ) return 
-if(!msg.quoted.caption) return console.log('ew')
-text = msg.quoted.caption
-if (!text.includes('*ᴅᴀʀᴋ ɴᴇʀᴏ ᴠ9 ᴍᴀɪɴ ᴄᴏᴍᴍᴀɴᴅ ʟɪꜱᴛ*'))  return 
-
-await Void.sendMessage(citel.chat, { react: {  text: "🤹‍♀️", key: msg.key } } )			
-
-    let alivemessage = Config.ALIVE_MESSAGE || `_*A bot developed by MR-HANSAMALA*_`
-    const alivtxt = `
-*ʜᴇʟʟᴏ ${citel.pushName}*
-┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉
-🤺⃟⃪⃭⃐🥷ᴜᴘᴛɪᴍᴇ⇨ ${runtime(process.uptime())}
-🤺⃟⃪⃭⃐🥷ᴍᴏᴅᴇ⇨ ${Config.WORKTYPE}
-┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉
-♤ᴍɪꜱᴄ ᴄᴏᴍᴍᴀɴᴅꜱ♤
-▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁
-| 🎋⃟🥷   ꜱᴇᴛᴡᴇʟᴄᴏᴍᴇ
-| 🎋⃟🥷   ꜱᴇᴛɢᴏᴏᴅʙʏᴇ
-| 🎋⃟🥷   ᴇxᴇᴄ
-| 🎋⃟🥷   ʀᴇᴀᴅᴍᴏʀᴇ
-| 🎋⃟🥷   ᴜᴘᴛɪᴍᴇ
-| 🎋⃟🥷   ᴡᴍ
-| 🎋⃟🥷   ᴘɪᴄᴋ
-| 🎋⃟🥷   ꜰʟɪᴘᴛᴇxᴛ
-| 🎋⃟🥷   ᴍᴘ4ꜰʀᴏᴍᴜʀʟ
-| 🎋⃟🥷   ᴇʙɪɴᴀʀʏ
-| 🎋⃟🥷   ʙᴏᴛ
-| 🎋⃟🥷   ᴜɴʙᴀɴ
-| 🎋⃟🥷   ᴛʀᴛ
-┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉
-©ᴅᴇᴠᴇʟᴏᴘᴇᴅ ʙʏ ᴄʏʙᴇʀ ʏᴀᴋᴜᴢᴀ ᴛᴇᴀᴍ
-━━━━━━━━━━━━━━━━
-`;
-let aliveMessage = {
-    image: {
-        url: await botpic(),
-    },
-    caption: alivtxt,
-    footer: tlang().footer,
-    headerType: 4,
-};
- return Void.sendMessage(citel.chat, aliveMessage, {
-    quoted: citel,
-});
-
-}catch(e){
-
-}}
-)
-
-
-
 
 
 cmd({
-    pattern: "5",
-alias: ["groupmenu", "5"],
+    pattern: "2",
+alias: ["groupmenu", "02"],
     react: "📑",
     category: "general",
     filename: __filename,
@@ -528,64 +241,8 @@ let aliveMessage = {
 
 
 cmd({
-    pattern: "6",
-alias: ["searchmenu", "6"],
-    react: "📑",
-    category: "general",
-    filename: __filename,
-    desc: "is bot alive??"
-},
-async(Void, citel, text) => {
-    try{
-var msg = citel	
-if(!msg.quoted) return 
-if (!msg.quoted.isBaileys ) return 
-if(!msg.quoted.caption) return console.log('ew')
-text = msg.quoted.caption
-if (!text.includes('*ᴅᴀʀᴋ ɴᴇʀᴏ ᴠ9 ᴍᴀɪɴ ᴄᴏᴍᴍᴀɴᴅ ʟɪꜱᴛ*'))  return 
-
-await Void.sendMessage(citel.chat, { react: {  text: "🤹‍♀️", key: msg.key } } )
-    let alivemessage = Config.ALIVE_MESSAGE || `_*A bot developed by MR-HANSAMALA*_`
-    const alivtxt = `
-*ʜᴇʟʟᴏ ${citel.pushName}*
-┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉
-🤺⃟⃪⃭⃐🥷ᴜᴘᴛɪᴍᴇ⇨ ${runtime(process.uptime())}
-🤺⃟⃪⃭⃐🥷ᴍᴏᴅᴇ⇨ ${Config.WORKTYPE}
-┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉
-♤ꜱᴇᴀʀᴄʜ ᴄᴏᴍᴍᴀɴᴅꜱ♤
-▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁
-| 🎋⃟🥷   ɴᴘᴍ
-| 🎋⃟🥷   ɪᴍᴅʙ
-| 🎋⃟🥷   ᴡᴇᴀᴛʜᴇʀ
-| 🎋⃟🥷   ʜᴏʀᴏ
-| 🎋⃟🥷   ɢᴏᴏɢʟᴇ
-| 🎋⃟🥷   ɪᴍɢ
-| 🎋⃟🥷   ᴄᴏᴜᴘʟᴇᴘᴘ
-| 🎋⃟🥷   ɪꜱᴡᴀ
-┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉
-©ᴅᴇᴠᴇʟᴏᴘᴇᴅ ʙʏ ᴄʏʙᴇʀ ʏᴀᴋᴜᴢᴀ ᴛᴇᴀᴍ
-━━━━━━━━━━━━━━━━
-`;
-let aliveMessage = {
-    image: {
-        url: await botpic(),
-    },
-    caption: alivtxt,
-    footer: tlang().footer,
-    headerType: 4,
-};
- return Void.sendMessage(citel.chat, aliveMessage, {
-    quoted: citel,
-});
-
-}catch(e){
-
-}}
-)
-
-cmd({
-    pattern: "7",
-alias: ["othermenu", "7"],
+    pattern: "3",
+alias: ["othermenu", "03"],
     react: "📑",
     category: "general",
     filename: __filename,
@@ -632,154 +289,3 @@ let aliveMessage = {
 
 }}
 )
-cmd({
-            pattern: "list",
-	    alias: [],
-            react: "🥷",
-            category: "general",
-            filename: __filename,
-            desc: "is bot alive??"
-        },
-        async(Void, citel, text, isAdmins) => {
-            let alivemessage = Config.ALIVE_MESSAGE || `_*A bot developed by MR HANSAMALA*_`
-            const alivtxt = `
-🔥 *ᴅᴀʀᴋ ɴᴇʀᴏ ᴠ9 ᴀʟʟ ᴄᴏᴍᴍᴀɴᴅ ʟɪꜱᴛ*🔥
-┏━━━━━━━━━━━━━━━┓
- ❀ *ʜᴇʟʟᴏ ᴡᴇʟᴄᴏᴍᴇ ᴛᴏ ᴅᴀʀᴋ ɴᴇʀᴏ ʙᴏᴛ ᴜꜱᴇʀꜱ*❀
-┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉
-❖ᴜᴘᴛɪᴍᴇ⇨ ${runtime(process.uptime())}
-❖ᴍᴏᴅᴇ⇨ ${Config.WORKTYPE}
-┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉
-      ♤ᴀʟʟ ᴄᴏᴍᴍᴀɴᴅꜱ♤
-▏▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁
-| 🎋⃟🥷   ɪɴꜱᴛᴀ
-| 🎋⃟🥷   ꜰʙ
-| 🎋⃟🥷   ꜰʙꜱ
-| 🎋⃟🥷   ᴠɪᴅᴇᴏ
-| 🎋⃟🥷   ꜱᴏɴɢ
-| 🎋⃟🥷   ᴛᴛꜱ
-| 🎋⃟🥷   ᴍᴇᴅɪᴀꜰɪʀᴇ
-| 🎋⃟🥷   ʏᴛꜱ
-| 🎋⃟🥷   ʏᴛᴍᴘ3
-| 🎋⃟🥷   ʏᴛᴍᴘ4
-| 🎋⃟🥷   ᴠɪᴅᴇᴏ2
-| 🎋⃟🥷   ʏᴛᴅᴏᴄ
-| 🎋⃟🥷   ᴅᴏᴡɴᴍᴘ4
-| 🎋⃟🥷   ᴘʟᴀʏʟɪꜱᴛ
-| 🎋⃟🥷   ᴛɪᴋᴛᴏᴋ
-| 🎋⃟🥷   ʟᴏʟɪ
-| 🎋⃟🥷   ɴᴇᴋᴏ
-| 🎋⃟🥷   ᴀɴɪᴍᴇ
-| 🎋⃟🥷   ᴍᴀɴɢᴀ
-| 🎋⃟🥷   ᴡᴀʟʟᴘᴀᴘᴇʀ
-| 🎋⃟🥷   ᴀɴɪᴍᴇɴᴇᴡꜱ
-| 🎋⃟🥷   ꜰᴏxɢɪʀʟ
-| 🎋⃟🥷   ɴᴀʀᴜᴛᴏ
-| 🎋⃟🥷   ᴡᴀɪꜰᴜ
-| 🎋⃟🥷   ᴘʟᴜɢɪɴꜱ
-| 🎋⃟🥷   ʀᴇᴍᴏᴠᴇ
-| 🎋⃟🥷   ɪɴꜱᴛᴀʟʟ
-| 🎋⃟🥷   ᴊᴏɪɴ
-| 🎋⃟🥷   ᴜɴʙʟᴏᴄᴋ
-| 🎋⃟🥷   ᴜᴊɪᴅ
-| 🎋⃟🥷   ʙʟᴏᴄᴋ
-| 🎋⃟🥷   ꜱᴀᴠᴇ
-| 🎋⃟🥷   ᴅɴᴏᴛᴇ
-| 🎋⃟🥷   ᴅᴀʟʟɴᴏᴛᴇ
-| 🎋⃟🥷   ɢᴇᴛ
-| 🎋⃟🥷   ꜱᴇᴛᴡᴇʟᴄᴏᴍᴇ
-| 🎋⃟🥷   ꜱᴇᴛɢᴏᴏᴅʙʏᴇ
-| 🎋⃟🥷   ᴇxᴇᴄ
-| 🎋⃟🥷   ʀᴇᴀᴅᴍᴏʀᴇ
-| 🎋⃟🥷   ᴜᴘᴛɪᴍᴇ
-| 🎋⃟🥷   ᴡᴍ
-| 🎋⃟🥷   ᴘɪᴄᴋ
-| 🎋⃟🥷   ꜰʟɪᴘᴛᴇxᴛ
-| 🎋⃟🥷   ᴍᴘ4ꜰʀᴏᴍᴜʀʟ
-| 🎋⃟🥷   ᴇʙɪɴᴀʀʏ
-| 🎋⃟🥷   ʙᴏᴛ
-| 🎋⃟🥷   ᴜɴʙᴀɴ
-| 🎋⃟🥷   ᴛʀᴛ
-| 🎋⃟🥷   ꜱᴛɪᴄᴋᴇʀ
-| 🎋⃟🥷   ᴡᴀʀɴ
-| 🎋⃟🥷   ᴛᴀɢᴀʟʟ
-| 🎋⃟🥷   ʀᴇQᴜᴇꜱᴛ
-| 🎋⃟🥷   ʀᴇᴛʀɪᴠᴇ
-| 🎋⃟🥷   ʀᴇꜱᴇᴛᴡᴀʀɴ
-| 🎋⃟🥷   ᴘᴏʟʟ
-| 🎋⃟🥷   ᴘʀᴏꜰɪʟᴇ
-| 🎋⃟🥷   ʀᴀɴᴋ
-| 🎋⃟🥷   ᴘʀᴏᴍᴏᴛᴇ
-| 🎋⃟🥷   ᴋɪᴄᴋ
-| 🎋⃟🥷   ɢʀᴏᴜᴘ
-| 🎋⃟🥷   ʙʀᴏᴅᴄᴀꜱᴛ
-| 🎋⃟🥷   ᴀɴᴛɪʟɪɴᴋ
-| 🎋⃟🥷   ᴀᴄᴛ
-| 🎋⃟🥷   ᴅᴇᴀᴄᴛ
-| 🎋⃟🥷   ɴᴘᴍ
-| 🎋⃟🥷   ɪᴍᴅʙ
-| 🎋⃟🥷   ᴡᴇᴀᴛʜᴇʀ
-| 🎋⃟🥷   ʜᴏʀᴏ
-| 🎋⃟🥷   ɢᴏᴏɢʟᴇ
-| 🎋⃟🥷   ɪᴍɢ
-| 🎋⃟🥷   ᴄᴏᴜᴘʟᴇᴘᴘ
-| 🎋⃟🥷   ɪꜱᴡᴀ
-| 🎋⃟🥷   ᴇꜱᴀɴᴀ
-| 🎋⃟🥷   ɴᴇᴡꜱ
-┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉
-©ᴅᴇᴠᴇʟᴏᴘᴇᴅ ʙʏ ᴄʏʙᴇʀ ʏᴀᴋᴜᴢᴀ ᴛᴇᴀᴍ
-━━━━━━━━━━━━━━━━
-`;
-            let aliveMessage = {
-                image: {
-                    url: await botpic(),
-                },
-                caption: alivtxt,
-                footer: tlang().footer,
-                headerType: 4,
-            };
-            Void.sendMessage(citel.chat, aliveMessage, {
-                quoted: citel,
-            });
-
-        }
-    )
-
-cmd({
-            pattern: "help",
-            react: "🥷",
-            category: "general",
-            filename: __filename,
-            desc: "is bot alive??"
-        },
-        async(Void, citel, text, isAdmins) => {
-            let alivemessage = Config.ALIVE_MESSAGE || `_*A bot developed by MR HANSAMALA*_`
-            const alivtxt = `
-🔥 *ᴅᴀʀᴋ ɴᴇʀᴏ ᴠ9*🔥
-┏━━━━━━━━━━━━━━━┓
- ❀ *ʜᴇʟʟᴏ ᴡᴇʟᴄᴏᴍᴇ ᴛᴏ ᴅᴀʀᴋ ɴᴇʀᴏ ʙᴏᴛ ᴜꜱᴇʀꜱ*❀
-┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉
-❖ᴜᴘᴛɪᴍᴇ⇨ ${runtime(process.uptime())}
-❖ᴍᴏᴅᴇ⇨ ${Config.WORKTYPE}
-┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉
-      ♤​🇨​​🇴​​🇲​​🇲​​🇮​​🇳​​🇬​ ​🇸​​🇴​​🇴​​🇳​♤
-
-▏▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁
-┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉
-©ᴅᴇᴠᴇʟᴏᴘᴇᴅ ʙʏ ᴄʏʙᴇʀ ʏᴀᴋᴜᴢᴀ ᴛᴇᴀᴍ
-━━━━━━━━━━━━━━━━
-`;
-            let aliveMessage = {
-                image: {
-                    url: await botpic(),
-                },
-                caption: alivtxt,
-                footer: tlang().footer,
-                headerType: 4,
-            };
-            Void.sendMessage(citel.chat, aliveMessage, {
-                quoted: citel,
-            });
-
-        }
-    )
