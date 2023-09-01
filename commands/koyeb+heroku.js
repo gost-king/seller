c9a3c29ba41f914d40d4ddc580ad1bbf7a48b79c
gost-king <143776715+gost-king@users.noreply.github.com>
2023-09-01 14:14:57 +0530

    Update koyeb+heroku.js

--- commands/koyeb+heroku.js
+++ commands/koyeb+heroku.js
@@ -1,32 +1,5 @@
 /**
-⢀⣀⠀⠀⠀⠰⣶⡖⠲⠦⠤⣿⣝⠲⣤⣄⡀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠸⣿⡉⠛⣒⠲⠾⡟⠓⠒⠲⡾⢿⣿⢿⡦⣍⠻⣷⣤⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠹⣟⢄⡈⠙⠢⠤⣀⣀⣀⣙⣦⣙⠻⢦⣌⡳⠄⣿⣮⣷⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⣿⡷⣝⡢⠄⢰⡚⠛⠓⠒⠤⠬⣿⡶⠬⡅⠘⢿⣷⠀⣻⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠹⣗⣤⣽⡦⠄⠙⢦⣀⠀⠀⠀⢻⠘⣆⢰⣒⣦⣤⣼⣍⡿⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠈⠙⣿⢯⣔⣒⣀⣈⠿⠒⣀⡤⠒⠋⠉⠁⠀⠀⠀⠉⣿⡘⢦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠿⠷⢶⡞⠋⠉⢩⠤⠞⠁⠀⠀⠀⢀⣀⣤⠤⠲⣞⣿⣷⠸⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⢸⣿⣀⣀⣀⣠⠤⠤⠖⢒⣿⠿⠟⠛⠂⠀⢹⣿⣿⠀⢧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠸⣿⣻⡠⠤⣤⣴⣦⡀⠉⠁⠴⣿⠟⠀⠀⠀⣿⠏⠀⠀⠑⢄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⢻⣿⣇⠀⠛⠛⠁⣇⣀⣤⡤⠤⠤⠤⢰⡾⠃⠀⠀⢀⣀⡈⢣⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⢻⣿⣤⡤⠖⣫⠵⢷⠚⠛⢻⠉⣹⠋⠀⣠⠔⢊⣁⡤⠤⠌⢧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠈⢿⣷⠚⠉⢱⠀⠸⡄⠀⣠⠟⠁⠴⣫⠔⠊⠁⠀⠀⠀⠀⠈⢦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⣿⣇⠀⠀⠇⠀⣱⠞⠁⠀⡠⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠳⣄⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⡿⣆⠀⢠⠞⠁⠀⡠⠊⠀⠀⠀⠀⠀⠀⢀⣀⣠⣤⠤⠔⠒⠚⠓⠒⠤⣄⡀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⣰⣿⣿⡿⠁⠈⡶⠃⢀⡴⠊⠀⠀⠀⠀⠀⣠⠴⠚⠉⣠⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢆⠀⠀
-⠀⠀⠀⠀⠀⠀⢰⣿⣿⠟⠀⢀⠞⠁⠐⠋⠀⠀⠀⠀⣠⢴⡎⠁⠀⠀⡴⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢧⠀
-⠀⠀⠀⠀⠀⠀⠈⢿⡇⠀⢠⣏⣠⠞⠀⠀⠀⢀⡴⠊⠁⠀⠳⠀⢀⡜⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⡇
-⠀⠀⠀⠀⠀⠀⠀⠘⣷⠀⠸⠈⠀⠀⢀⡤⠖⠉⠀⣠⠆⠀⠀⢀⡞⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇
-⠀⠀⠀⠀⠀⢀⡴⠋⢹⣆⣀⣠⣤⠒⠙⡇⣠⠔⠋⠀⠀⠀⠀⣸⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇
-⠀⠀⠀⠀⠀⡞⠀⢀⠼⡇⢰⣾⣿⣷⡴⣟⠁⠀⠀⠀⠀⠀⣰⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇
-⠀⠀⠀⠀⠀⣇⡴⢁⡴⣇⣨⣼⣿⠋⠀⠈⠀⠀⠀⠀⢀⡼⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠃
-⠀⠀⠀⠀⠀⠉⠀⠈⠀⠈⠀⠀⠉⠀⠀⠀⠀⠀⠀⠀⠈⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
- Copyright (C) 2023
- Licensed under the  GPL-3.0 License;
- You may not use this file except in compliance with the License.
- It is supplied in the hope that it may be useful.
- * @project_name : Dark Nero V9
- * @description : Dark Nero V9,A Multi-functional whatsapp bot.
+
  **/
 const axios = require('axios');
 const { tlang,cmd } = require('../lib')
@@ -87,76 +60,5 @@ cmd(
        let check = await get_deployments()
        if(check==='true') return citel.reply('_Please wait..._\n_Currently 2 instances are running in Koyeb,wait to stop one of them._')
        let data = await redeploy();
-       return citel.reply(data)
-  })
-//----------------------------------------------------------------------------------------------------------------------------------------------------
-cmd(
-  {
-    pattern: "getvar",
-    desc: "get desired var from koyeb.",
-    react: "🎆",
-    filename: __filename,
-    category: "misc",
-  },
-  async (Void,citel,text,{isCreator}) => {
-       if(!isCreator) return citel.reply(tlang().owner);
-       if (Config.heroku=true){
-        if (!text.split(' ')[0]) return citel.reply(`Please Give me var name, ${tlang().greet}`)
-        const Heroku = require("heroku-client");
-        const heroku = new Heroku({
-          token: Config.HEROKU.API_KEY,
-        });
-        let baseURI = "/apps/" + Config.HEROKU.APP_NAME;
-  let h = await heroku.get(baseURI+'/config-vars')
-  for (vr in h) {
-  if(text.split(' ')[0]===vr) return citel.reply('Your var.\n'+vr+': '+h[vr])	
-  }
-       }
-       if(!text) return citel.reply('Please provide key.\n_Eg: .getvar PORT_')
-       let data = await getvar(text);
-       return citel.reply(data)
-  })
-//----------------------------------------------------------------------------------------------------------------------------------------------------
-cmd(
-  {
-    pattern: "getallvar",
-    desc: "get all vars from koyeb.",
-    react: "🌠",
-    filename: __filename,
-    category: "misc",
-  },
-  async (Void,citel,text,{isCreator}) => {
-       if(!isCreator) return citel.reply(tlang().owner);
-       if (Config.heroku=true){
-        const Heroku = require("heroku-client");
-			const heroku = new Heroku({
-				token: Config.HEROKU.API_KEY,
-			});
-			let baseURI = "/apps/" + Config.HEROKU.APP_NAME;
-let h = await heroku.get(baseURI+'/config-vars')
-let str = '*All Your Heroku Vars*\n\n'
-for (vr in h) {
-str+= vr+':'+h[vr]+'\n'
-}
-citel.reply(str)
-       }
-       let data = await getallvar();
-       return citel.reply(data)
-  })
-//----------------------------------------------------------------------------------------------------------------------------------------------------
-cmd(
-  {
-    pattern: "delvar",
-    desc: "delete var from koyeb.",
-    react: "🎠",
-    filename: __filename,
-    category: "misc",
-  },
-  async (Void,citel,text,{isCreator}) => {
-       if(!isCreator) return citel.reply(tlang().owner);
-       if(!text) return citel.reply('Please provide key.\n_Eg: .delvar PORT_')
-       let check = await get_deployments()
-       if(check==='true') return citel.reply('_Please wait..._\n_Currently 2 instances are running in Koyeb,wait to stop one of them._')
-       let data = await delvar(text)
        return citel.reply(data)
   })
