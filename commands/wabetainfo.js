7ee07803f46289c0457a7e2ff4d0f6cd236d6566
gost-king <143776715+gost-king@users.noreply.github.com>
2023-09-02 10:11:11 +0530

    Create wabetainfo.js

---
+++ commands/wabetainfo.js
@@ -0,0 +1,27 @@
+cmd({
+    pattern: "wabeta",
+    alias: ["wabetaino",".wabe"],
+    react: "🧾",
+    desc: "",
+    category: "download",
+    use: '.wabetainfo',
+    filename: __filename
+},
+async(conn, mek, m,{from, l, quoted, body, isCmd, darkneo, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+
+const wanews = await fetchJson(`https://vihangayt.me/details/wabetainfo?apikey=VihangaYT`);
+
+
+         
+
+          const images = `${wanews.result.image}`
+           const title = `${wanews.result.title}`
+           const news = `${wanews.result.fulldesc}`
+           const date = `${wanews.result.date}`
+
+await conn.sendMessage(from,  { image: { url: images }, caption: `\n\n*${ title }*\n\n\n ${ news }\n\n\n ${date}`}, { quoted: mek })
+}
+catch(e){
+console.log(e)
+}})
